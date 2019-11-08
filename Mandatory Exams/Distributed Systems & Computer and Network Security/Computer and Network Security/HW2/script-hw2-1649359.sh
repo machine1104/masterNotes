654bd9f28c071c14eada849dc4a2b092a0b5034b
Edoardo Puglisi <machine1104@gmail.com>
2019-11-08 21:27:59 +0100

    Fixed script

--- Mandatory Exams/Distributed Systems & Computer and Network Security/Computer and Network Security/HW2/script-hw2-1649359.sh
+++ Mandatory Exams/Distributed Systems & Computer and Network Security/Computer and Network Security/HW2/script-hw2-1649359.sh
@@ -1,8 +1,8 @@
 !/bin/bash
 
 echo ++++++ HOMEWORK 2 ++++++
-start=$(date +%H%M%S%N)
-for dict in en
+start=$(date +%s)
+for dict in en it
 do
     while read -r line
     do
@@ -10,12 +10,10 @@ do
         openssl aes-192-cbc -d -in "ciphertext-hw2-1649359.enc" -out "plaintext.txt" -pass pass:$line -pbkdf2 2> /dev/null
         if [[ $(file plaintext.txt | cut -d' ' -f2) == "ASCII" ]];
         then  
-            echo DAJE
-            end=$(date +%H%M%S%N)
-            runtime="$(bc <<< "scale=3;$end-$start")"
-            runtime_scaled=$(bc <<< "scale=3;$runtime/1000000")
-            echo $runtime_scaled       
+            runtime=$(($(date +%s)-$start))
+            printf "La passowrd è $line\n"            
+            printf "Daje! Hai sprecato ben %dh:%dm:%ds. della tua vita\n" $(($runtime/3600)) $(($runtime%3600/60)) $(($runtime%60))     
             exit
         fi
-    done < "$dict-hw2-1649359.dic"
+    done < "$dict-prova.txt"
 done
