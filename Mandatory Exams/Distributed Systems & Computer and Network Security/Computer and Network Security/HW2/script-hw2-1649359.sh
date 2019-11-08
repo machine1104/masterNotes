ac190e94f380ad55adba79a27c9a4999bf5445d6
Edoardo Puglisi <machine1104@gmail.com>
2019-11-08 21:08:45 +0100

    Update script-hw2-1649359.sh

--- Mandatory Exams/Distributed Systems & Computer and Network Security/Computer and Network Security/HW2/script-hw2-1649359.sh
+++ Mandatory Exams/Distributed Systems & Computer and Network Security/Computer and Network Security/HW2/script-hw2-1649359.sh
@@ -1,52 +1,21 @@
-#!/bin/bash
+!/bin/bash
 
-PASSWORD="passwordmoltosicura"
-OUTPUT="result.hw1-1649359.csv"
-CICLI=10
-type nul > $OUTPUT
-
-echo ++++++ HOMEWORK 1 ++++++
-
-for cipher in aes-256-cbc aes-256-ecb camellia-256-cbc camellia-256-ecb aria-256-cbc aria-256-ecb des-cbc des-ecb
+echo ++++++ HOMEWORK 2 ++++++
+start=$(date +%H%M%S%N)
+for dict in en
 do
-    echo $cipher >>  $OUTPUT
-    echo +++++ CIPHER $cipher +++++    
-    for size in 100K 1MB 10MB 100MB
+    while read -r line
     do
-        echo +++++ FILE SIZE $size +++++
-        echo ===== ENCRYPTION =====       
-        
-        runtime=0
-        for i in {1..$CICLI}
-        do
-            start=$(date +%s%N)
-            openssl $cipher -a -salt -pbkdf2 -in "$size-hw1-1649359.file" -out "$size.enc" -pass pass:$PASSWORD
-            runtime="$(($runtime+($(date +%s%N)-$start)))"       
-           
-        done
-
-        enc_runtime_scaled=$(bc <<< "scale=3;$runtime/$CICLI/1000000")
-        echo ENC_SPEED $enc_runtime_scaled
-
-        echo ===== DECRYPTION =====       
-        
-        runtime=0
-        for i in {1..$CICLI}
-        do
-            start=$(date +%s%N)
-            openssl $cipher -d -a -salt -pbkdf2 -in "$size.enc" -out "$size.new.file" -pass pass:$PASSWORD
-            runtime="$(($runtime+($(date +%s%N)-$start)))"       
-           
-        done
-
-        dec_runtime_scaled=$(bc <<< "scale=3;$runtime/$CICLI/1000000")
-        echo DEC_SPEED $dec_runtime_scaled
-        echo $size >> $OUTPUT
-        echo ENC";"$enc_runtime_scaled >> $OUTPUT
-        echo DEC";"$dec_runtime_scaled >> $OUTPUT
-
-        
-              
-    done    
+        echo -e $line
+        openssl aes-192-cbc -d -in "ciphertext-hw2-1649359.enc" -out "plaintext.txt" -pass pass:$line -pbkdf2 2> /dev/null
+        if [[ $(file plaintext.txt | cut -d' ' -f2) == "ASCII" ]];
+        then  
+            echo DAJE
+            end=$(date +%H%M%S%N)
+            runtime="$(bc <<< "scale=3;$end-$start")"
+            runtime_scaled=$(bc <<< "scale=3;$runtime/1000000")
+            echo $runtime_scaled       
+            exit
+        fi
+    done < "$dict-hw2-1649359.dic"
 done
-
