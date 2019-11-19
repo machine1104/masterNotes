e66d5aef948828ff1f8e0a759e3d2be7997c2dab
Edoardo Puglisi <machine1104@gmail.com>
2019-11-19 09:47:40 +0100

    Added charts

--- Mandatory Exams/Distributed Systems & Computer and Network Security/Computer and Network Security/HW3/script-hw3-1649359.sh
+++ Mandatory Exams/Distributed Systems & Computer and Network Security/Computer and Network Security/HW3/script-hw3-1649359.sh
@@ -3,7 +3,7 @@
 PASSWORD="passwordmoltosicura"
 OUTPUT="result.hw3-1649359.csv"
 CICLI=10
-type nul > $OUTPUT
+truncate -s 0 $OUTPUT
 
 echo ++++++ HOMEWORK 3 ++++++
 
@@ -14,7 +14,7 @@ do
     for text in text1 text2 text3
     do
         echo ===== ENCRYPTION =====       
-        
+        echo $text >>  $OUTPUT
         runtime=0
         for i in {1..$CICLI}
         do
@@ -42,46 +42,62 @@ do
         echo DEC_SPEED $dec_runtime_scaled
         echo $size >> $OUTPUT
         echo ENC";"$enc_runtime_scaled >> $OUTPUT
-        echo DEC";"$dec_runtime_scaled >> $OUTPUT     
+        echo DEC";"$dec_runtime_scaled >> $OUTPUT
               
     done 
-    
-    
 
-    # for bin in 1024 10240 102400
-    #     dd if=/dev/urandom of=$bin.file bs=$bin count=$bin   
-    # do
-    #     echo ===== ENCRYPTION =====       
-        
-    #     runtime=0
-    #     for i in {1..$CICLI}
-    #     do
-    #         start=$(date +%s%N)
-    #         openssl $cipher -a -salt -pbkdf2 -in "$bin-hw3-1649359.file" -out "$bin.enc" -pass pass:$PASSWORD
-    #         runtime="$(($runtime+($(date +%s%N)-$start)))"       
+    for bin in 1024 3238 10240
+        do
+        dd if="/dev/urandom" of="$bin.file" bs="$bin" count="$bin"
+        echo ===== ENCRYPTION =====       
+        echo $bin >>  $OUTPUT
+        runtime=0
+        for i in {1..$CICLI}
+        do
+            start=$(date +%s%N)
+            openssl $cipher -a -salt -pbkdf2 -in "$bin.file" -out "$bin.enc" -pass pass:$PASSWORD
+            runtime="$(($runtime+($(date +%s%N)-$start)))"       
            
-    #     done
+        done
 
-    #     enc_runtime_scaled=$(bc <<< "scale=3;$runtime/$CICLI/1000000")
-    #     echo ENC_SPEED $enc_runtime_scaled
+        enc_runtime_scaled=$(bc <<< "scale=3;$runtime/$CICLI/1000000")
+        echo ENC_SPEED $enc_runtime_scaled
 
-    #     echo ===== DECRYPTION =====       
+        echo ===== DECRYPTION =====       
         
-    #     runtime=0
-    #     for i in {1..$CICLI}
-    #     do
-    #         start=$(date +%s%N)
-    #         openssl $cipher -d -a -salt -pbkdf2 -in "$bin.enc" -out "$bin.new.file" -pass pass:$PASSWORD
-    #         runtime="$(($runtime+($(date +%s%N)-$start)))"       
+        runtime=0
+        for i in {1..$CICLI}
+        do
+            start=$(date +%s%N)
+            openssl $cipher -d -a -salt -pbkdf2 -in "$bin.enc" -out "$bin.new.file" -pass pass:$PASSWORD
+            runtime="$(($runtime+($(date +%s%N)-$start)))"       
            
-    #     done
+        done
 
-    #     dec_runtime_scaled=$(bc <<< "scale=3;$runtime/$CICLI/1000000")
-    #     echo DEC_SPEED $dec_runtime_scaled
-    #     echo $size >> $OUTPUT
-    #     echo ENC";"$enc_runtime_scaled >> $OUTPUT
-    #     echo DEC";"$dec_runtime_scaled >> $OUTPUT     
+        dec_runtime_scaled=$(bc <<< "scale=3;$runtime/$CICLI/1000000")
+        echo DEC_SPEED $dec_runtime_scaled
+        echo $size >> $OUTPUT
+        echo ENC";"$enc_runtime_scaled >> $OUTPUT
+        echo DEC";"$dec_runtime_scaled >> $OUTPUT     
               
-    # done    
+    done
+
+    
 done
+
+for bin in 1024 3238 10240
+        do
+        rm "$bin.file"
+        rm "$bin.enc"
+        rm "$bin.new.file"
+done
+
+for text in text1 text2 text3
+    do
+    rm "$text.enc"
+    rm "$text.new.txt"
+done
+
+
+
 
